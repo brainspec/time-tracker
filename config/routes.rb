063118f698f914e652b2d255116225c83960e5fe
Sergey Nartimov <just.lest@gmail.com>
2012-04-06 11:26:56 +0300

    add time entry routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ TimeTracker::Application.routes.draw do
   resource :session, only: %w[new destroy]
   match '/auth/:provider/callback', to: 'sessions#create'
 
+  resources :time_entries, only: 'create'
+
   root to: 'home#index'
 end

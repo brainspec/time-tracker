49c2882744d9102fb6f6b4ec21c75fc469ce497d
Sergey Nartimov <just.lest@gmail.com>
2012-04-06 10:29:46 +0300

    add token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User
   field :uid,  type: String
   field :name, type: String
 
+  field :token, type: String
+
   class << self
     def find_or_create_by_auth_hash(auth_hash)
       User.find_or_initialize_by(uid: auth_hash.uid).tap do |user|
-        user.name = auth_hash.info.name
+        user.name  = auth_hash.info.name
+        user.token = auth_hash.credentials.token
         user.save!
       end
     end

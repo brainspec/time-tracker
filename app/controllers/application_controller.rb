d472622faf95e76e0e626742d83ca0740d959ba3
Sergey Nartimov <just.lest@gmail.com>
2012-04-06 14:15:11 +0300

    fix when user not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= session[:user_id] && User.find(session[:user_id])
+  rescue Mongoid::Errors::DocumentNotFound
+    session.clear
+
+    nil
   end
 
   def require_user

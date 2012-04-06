6b814b34c10d0f7c1bff9cedd9c7fa6fcc6c7d8f
Vasiliy Ermolovich <younash@gmail.com>
2012-04-06 16:41:34 +0300

    use new hash syntax

--- spec/models/time_entry_spec.rb
+++ spec/models/time_entry_spec.rb
@@ -5,7 +5,7 @@ describe TimeEntry do
     let(:user) { Fabricate(:user) }
 
     it 'validates today total time overdue' do
-      time_entry = Fabricate.build(:time_entry, :user => user, :hours => 17)
+      time_entry = Fabricate.build(:time_entry, user: user, hours: 17)
       time_entry.save
 
       time_entry.errors[:hours].should include("Today total time is overdued by 1.0 hours")
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,11 @@ describe User do
 
     it 'returns total time of work that was done today' do
       Delorean.time_travel_to("1 day ago") do
-        Fabricate(:time_entry, :user => user, :hours => 10)
+        Fabricate(:time_entry, user: user, hours: 10)
       end
 
-      Fabricate(:time_entry, :user => user, :hours => 4)
-      Fabricate(:time_entry, :user => user, :hours => 5)
+      Fabricate(:time_entry, user: user, hours: 4)
+      Fabricate(:time_entry, user: user, hours: 5)
 
       user.today_total_time.should be_within(0.1).of(9.0)
     end

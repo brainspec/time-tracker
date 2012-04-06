07b79b93d2265c2f1e81492e216997f90a3c0b3a
Vasiliy Ermolovich <younash@gmail.com>
2012-04-06 10:06:29 +0300

    fix expand path in config.rb

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 
-config_file = File.expand_path('../../../config/config.yml')
+config_file = File.expand_path('../../../config/config.yml', __FILE__)
 if File.exists?(config_file)
   YAML.load_file(config_file).each_pair do |key, value|
     ENV[key.upcase] = value

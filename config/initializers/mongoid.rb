3ac2b078bf29d7c9b753b9ef35c670a2551013b3
Sergey Nartimov <just.lest@gmail.com>
2012-04-06 13:11:36 +0300

    heroku support

---
+++ config/initializers/mongoid.rb
@@ -0,0 +1,9 @@
+if mongo = ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI']
+  settings = URI.parse(mongo)
+  database_name = settings.path.gsub(/^\//, '')
+
+  Mongoid.configure do |config|
+    config.master = Mongo::Connection.new(settings.host, settings.port).db(database_name)
+    config.master.authenticate(settings.user, settings.password) if settings.user
+  end
+end

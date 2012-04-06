87450ee409be64d7d797fc6c08661f676afbae6c
Vasiliy Ermolovich <younash@gmail.com>
2012-04-06 11:30:39 +0300

    move app id to config, add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
 
   before_filter :require_user
 
+  helper_method :current_user
+
   private
 
+  def current_user
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
+  end
+
   def require_user
-    @current_user = session[:user_id] && User.find(session[:user_id])
-    redirect_to new_session_path unless @current_user
+    redirect_to new_session_path unless current_user
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,27 @@
 class HomeController < ApplicationController
   def index
-    conn = Faraday.new(:url => 'https://basecamp.com/') do |builder|
-      builder.response :logger
-      builder.use Faraday::Request::JSON
-      builder.use FaradayMiddleware::Mashify
-      builder.use FaradayMiddleware::ParseJson
-      builder.adapter  :net_http
-    end
+    if current_user
+      conn = Faraday.new(:url => 'https://basecamp.com/') do |builder|
+        builder.response :logger
+        builder.use Faraday::Request::JSON
+        builder.use FaradayMiddleware::Mashify
+        builder.use FaradayMiddleware::ParseJson
+        builder.adapter  :net_http
+      end
 
-    response = conn.get do |req|
-      req.url '/1785573/api/v1/people/me.json'
-      req.headers['Authorization'] = "Bearer #{@current_user.token}"
-      req.headers['User-Agent'] = "TimeTracker"
-    end
+      response = conn.get do |req|
+        req.url "/#{ENV['ACCOUNT_ID']}/api/v1/people/me.json"
+        req.headers['Authorization'] = "Bearer #{@current_user.token}"
+        req.headers['User-Agent'] = "TimeTracker"
+      end
 
-    response = conn.get do |req|
-      req.url "/1785573/api/v1/people/#{response.body.id}/assigned_todos.json"
-      req.headers['Authorization'] = "Bearer #{@current_user.token}"
-      req.headers['User-Agent'] = "TimeTracker"
-    end
+      response = conn.get do |req|
+        req.url "/#{ENV['ACCOUNT_ID']}/api/v1/people/#{response.body.id}/assigned_todos.json"
+        req.headers['Authorization'] = "Bearer #{@current_user.token}"
+        req.headers['User-Agent'] = "TimeTracker"
+      end
 
-    @todo_lists = response.body
+      @todo_lists = response.body
+    end
   end
 end

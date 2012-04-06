542b582141054bbe1a9a4c9b9c1194741220cd92
Vasiliy Ermolovich <younash@gmail.com>
2012-04-06 11:18:45 +0300

    add todo view

--- Gemfile
+++ Gemfile
@@ -24,6 +24,24 @@ gem 'omniauth-37signals'
 
 gem 'slim'
 
+gem 'faraday'
+gem 'faraday_middleware'
+
 group :development, :test do
   gem 'rspec-rails'
 end
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,25 @@
 class HomeController < ApplicationController
+  def index
+    conn = Faraday.new(:url => 'https://basecamp.com/') do |builder|
+      builder.response :logger
+      builder.use Faraday::Request::JSON
+      builder.use FaradayMiddleware::Mashify
+      builder.use FaradayMiddleware::ParseJson
+      builder.adapter  :net_http
+    end
+
+    response = conn.get do |req|
+      req.url '/1785573/api/v1/people/me.json'
+      req.headers['Authorization'] = "Bearer #{@current_user.token}"
+      req.headers['User-Agent'] = "TimeTracker"
+    end
+
+    response = conn.get do |req|
+      req.url "/1785573/api/v1/people/#{response.body.id}/assigned_todos.json"
+      req.headers['Authorization'] = "Bearer #{@current_user.token}"
+      req.headers['User-Agent'] = "TimeTracker"
+    end
+
+    @todo_lists = response.body
+  end
 end

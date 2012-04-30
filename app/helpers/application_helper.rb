59bc7980079171ae144d96fc33f355f4d409c868
Sergey Nartimov <just.lest@gmail.com>
2012-04-30 12:29:48 +0300

    refactor helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
-  def basecamp_url(url)
+  def basecamp_url(obj)
+    url = obj.respond_to?(:url) ? obj.url : obj.to_s
     url.gsub(/\.json$/, '').gsub('/api/v1', '')
   end
 
-  def project_by_todo_list_url(url)
+  def todo_list_project(todo_list)
     @projects ||= Basecamp.new(current_token).projects
 
-    bcx_id = url.match(/\/projects\/(\d+)/).captures.first.to_i
+    bcx_id = todo_list.url.match(/\/projects\/(\d+)/).captures.first.to_i
     @projects.find { |p| p.id == bcx_id }
   end
 end

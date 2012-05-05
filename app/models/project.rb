6f08905c3557713170c8d2c35652944be99f698c
Sergey Nartimov <just.lest@gmail.com>
2012-05-05 16:06:47 +0300

    add project name

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,15 @@ class Project
   include Mongoid::Document
 
   field :bcx_id, type: Integer
+  field :name,   type: String
 
   has_many :todos
 
   class << self
     def find_or_create_by_bcx_hash(bcx_hash)
-      find_or_initialize_by(bcx_id: bcx_hash.id).tap do |todo|
-        todo.save!
+      find_or_initialize_by(bcx_id: bcx_hash.id).tap do |project|
+        project.name = bcx_hash.name
+        project.save!
       end
     end
   end

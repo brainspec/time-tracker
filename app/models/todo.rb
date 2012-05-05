70a2080f6eac38c3501c5018434f8a70bb032dca
dreamfall <amarant.st@gmail.com>
2012-05-05 15:25:52 +0300

    add first comment and title fields to todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,11 @@
 class Todo
   include Mongoid::Document
 
-  field :project_id, type: Integer
-  field :bcx_id, type: Integer
-  field :created_at, type: Time
+  field :project_id,    type: Integer
+  field :bcx_id,        type: Integer
+  field :created_at,    type: Time
+  field :title,         type: String
+  field :first_comment, type: String
 
   belongs_to :project
 
@@ -12,7 +14,9 @@ class Todo
   class << self
     def find_or_create_by_bcx_hash(bcx_hash)
       find_or_initialize_by(bcx_id: bcx_hash.id).tap do |todo|
-        todo.created_at = bcx_hash.created_at
+        todo.created_at    = bcx_hash.created_at
+        todo.title         = bcx_hash.content
+        todo.first_comment = bcx_hash.comments.first.content
         todo.save!
       end
     end

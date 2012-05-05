076748a4d8f12f6c8c658be10844e486922f6054
Sergey Nartimov <just.lest@gmail.com>
2012-05-05 13:28:38 +0300

    method to find/create todo from bcx data

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,13 @@
 class Todo
   include Mongoid::Document
+
+  field :bcx_id, type: Integer
+
+  class << self
+    def find_or_create_by_bcx_hash(bcx_hash)
+      Todo.find_or_initialize_by(bcx_id: bcx_hash.id).tap do |todo|
+        todo.save!
+      end
+    end
+  end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Todo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.find_or_create_by_bcx_hash' do
+    let :bcx_hash do
+      data = JSON.parse File.read("#{Rails.root}/spec/assets/todo.json")
+      Hashie::Mash.new(data)
+    end
+
+    it 'creates todo' do
+      todo = Todo.find_or_create_by_bcx_hash(bcx_hash)
+      todo.bcx_id.should == 1
+    end
+
+    it 'updates todo' do
+      todo = double
+      todo.should_receive(:save!)
+      Todo.should_receive(:find) { todo }
+      Todo.find_or_create_by_bcx_hash(bcx_hash).should == todo
+    end
+  end
 end
